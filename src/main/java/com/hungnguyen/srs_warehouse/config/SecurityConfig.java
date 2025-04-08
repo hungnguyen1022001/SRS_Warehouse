273495b62e8173d1d14e2275ca2c79698038b28a
hwngngueyn <hunggithub.work@gmail.com>
2025-04-08 16:48:41 +0700

    update

--- src/main/java/com/hungnguyen/srs_warehouse/config/SecurityConfig.java
+++ src/main/java/com/hungnguyen/srs_warehouse/config/SecurityConfig.java
@@ -74,7 +74,7 @@ public class SecurityConfig {
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
-        configuration.setExposedHeaders(List.of("Authorization", "Content-Type"));
+        configuration.setExposedHeaders(List.of("Authorization", "Content-Type","Content-Disposition"));
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
